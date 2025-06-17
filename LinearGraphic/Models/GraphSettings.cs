b583ff463195f6ff77a9dd9a95b853650718fa2b
maximgf <maxim-a90@mail.ru>
2025-06-17 12:56:31 +0800

    INotify

--- LinearGraphic/Models/GraphSettings.cs
+++ LinearGraphic/Models/GraphSettings.cs
@@ -1,47 +1,108 @@
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Model;
 
 /// <summary>
 /// Настройки графиков с поддержкой уведомлений об изменениях
 /// </summary>
-public class GraphSettings: INotifyPropertyChanged
+public class GraphSettings : INotifyPropertyChanged
 {
+    private bool _extrapolation;
+    private int _displayPoints;
+    private int _chartYLevelMin;
+    private int _chartYLevelMax;
+    private int _chartXLevelMin;
+    private int _chartXLevelMax;
+
     /// <summary>
     /// Использовать интерполяцию
     /// </summary>
     [Description("Использовать интерполяцию")]
-    public bool Extrapolation { get; set; }
+    public bool Extrapolation
+    {
+        get => _extrapolation;
+        set
+        {
+            _extrapolation = value;
+            OnPropertyChanged();
+        }
+    }
 
     /// <summary>
     /// Количество отображаемых точек на графике
     /// </summary>
     [Description("Количество точек")]
-    public int DisplayPoints { get; set; }
+    public int DisplayPoints
+    {
+        get => _displayPoints;
+        set
+        {
+            _displayPoints = value;
+            OnPropertyChanged();
+        }
+    }
 
     /// <summary>
     /// Минимальный уровень графика по оси Y
     /// </summary>
     [Description("Минимальный уровень графика по оси Y")]
-    public int ChartYLevelMin { get; set; }
+    public int ChartYLevelMin
+    {
+        get => _chartYLevelMin;
+        set
+        {
+            _chartYLevelMin = value;
+            OnPropertyChanged();
+        }
+    }
 
     /// <summary>
     /// Максимальный уровень графика по оси Y
     /// </summary>
     [Description("Максимальный уровень графика по оси Y")]
-    public int ChartYLevelMax { get; set; }
+    public int ChartYLevelMax
+    {
+        get => _chartYLevelMax;
+        set
+        {
+            _chartYLevelMax = value;
+            OnPropertyChanged();
+        }
+    }
 
     /// <summary>
     /// Минимальный уровень графика по оси X
     /// </summary>
     [Description("Минимальный уровень графика по оси X")]
-    public int ChartXLevelMin { get; set; }
+    public int ChartXLevelMin
+    {
+        get => _chartXLevelMin;
+        set
+        {
+            _chartXLevelMin = value;
+            OnPropertyChanged();
+        }
+    }
 
     /// <summary>
     /// Максимальный уровень графика по оси X
     /// </summary>
     [Description("Максимальный уровень графика по оси X")]
-    public int ChartXLevelMax { get; set; }
+    public int ChartXLevelMax
+    {
+        get => _chartXLevelMax;
+        set
+        {
+            _chartXLevelMax = value;
+            OnPropertyChanged();
+        }
+    }
 
-    public event PropertyChangedEventHandler? PropertyChanged;
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
--- LinearGraphic/Models/PointsContext.cs
+++ LinearGraphic/Models/PointsContext.cs
@@ -1,23 +1,54 @@
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Model;
 
 public class PointsContext : INotifyPropertyChanged
 {
-    private Point[] arr { get; }
+    private Point[] _arr;
+    private int _count;
 
     public PointsContext(int size)
     {
-        arr = new Point[size];
+        _arr = new Point[size];
+        _count = 0;
     }
 
-    private int count = 0;
+    public Point[] Arr
+    {
+        get => _arr;
+        private set
+        {
+            _arr = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int Count
+    {
+        get => _count;
+        private set
+        {
+            _count = value;
+            OnPropertyChanged();
+        }
+    }
 
     public void Add(Point point)
     {
-        arr[count] = point;
-        count++;
+        if (_count < _arr.Length)
+        {
+            _arr[_count] = point;
+            _count += 1; // Используем свойство для обновления Count
+            Count = _count;
+            OnPropertyChanged(nameof(Arr)); // Уведомляем об изменении массива
+        }
     }
 
-    public event PropertyChangedEventHandler? PropertyChanged;
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
